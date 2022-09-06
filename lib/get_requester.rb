1111816ee899db1242d480a178e7d781aed21fb9
Evans Rutto <robbyevans001@gmail.com>
2022-09-06 14:33:54 +0300

    commit tree de10c2135ea311f81cbefe2b79be231239401fe1

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  attr_accessor :link
+
+  def initialize(link='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+    @url=link
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program["agency"]
+    end
+  end
+
+end
